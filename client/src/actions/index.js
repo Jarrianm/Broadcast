eed2f03fdd094ce12b880c29ae0aba7d628eda1f
JarrianM <jarrian.mclean@gmail.com>
2020-09-05 15:59:45 -0400

    testing redux-thunk current_user

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-const fetchUser = () => {
-  return function (dipatch) {
-    axios.get("/api/current_user").then((res) =>
-      dispatchEvent({
+export const fetchUser = () => {
+  return function (dispatch) {
+    axios
+    .get("/api/current_user")
+    .then(res =>
+      dispatch({
         type: FETCH_USER,
         payload: res,
       })
