6796109f715c668011f8d75ffeb7be7c4267b3e5
JarrianM <jarrian.mclean@gmail.com>
2020-09-05 13:59:26 -0400

    flow check

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,9 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from './components/App'
+import {Provider} from 'react-redux'
+import{ createStore, applyMiddleware} from 'redux'
 
+const store = createStore(() => [], {}, applyMiddleware());
 
-
-
-ReactDom.render(<App/>, document.querySelector('#root'));
+ReactDom.render(<Provider store={store}></Provider> <App/><Provider/>, document.querySelector('#root'));
