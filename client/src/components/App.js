a4335c81b62cec695f769ee129ec484d0554fd1e
JarrianM <jarrian.mclean@gmail.com>
2020-09-05 14:19:31 -0400

    setting up reducers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                sup!!!
-            </div>
-        );
-    }
+  render() {
+    return <div>sup!!!</div>;
+  }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 //rendering root component to the dom/redux
 import React from "react";
 import ReactDom from "react-dom";
-import App from './components/App'
-import {Provider} from 'react-redux'
-import{ createStore, applyMiddleware} from 'redux'
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
 
 const store = createStore(() => [], {}, applyMiddleware());
 
-ReactDom.render(<Provider store={store}> <App/></Provider>, document.querySelector('#root'));
+ReactDom.render(
+  <Provider store={store}>
+    {" "}
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
